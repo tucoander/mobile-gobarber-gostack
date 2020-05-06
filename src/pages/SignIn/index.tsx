d8e2d58a0f353a205ecb8bef439a769686002716
Anderson Martins <tuco.anderson@gmail.com>
2020-05-06 20:26:25 -0300

    criação do signin

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+     <Text>Oi</Text>
+    </Container>)
+};
+
+export default SignIn;
