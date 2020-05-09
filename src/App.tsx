2e39bb3f8e71072d46fff46516bb952c7e995967
Anderson Martins <tuco.anderson@gmail.com>
2020-05-09 09:26:28 -0300

    integrando o provider no app

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 import {View, StatusBar} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+import AppProvider from './hooks';
+
 import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content"></StatusBar>
-    <View style={{ flex: 1,backgroundColor: '#321e38'}}>
-      <Routes></Routes>
-    </View>
+    <AppProvider>
+      <View style={{ flex: 1,backgroundColor: '#321e38'}}>
+        <Routes></Routes>
+      </View>
+    </AppProvider>
   </NavigationContainer>
 );
 
