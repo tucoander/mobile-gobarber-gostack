fcca7602cb482d0fabf1a22ca7800b6a06609f1f
Anderson Martins <tuco.anderson@gmail.com>
2020-05-09 10:15:34 -0300

    integrando signout na pagina logada

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Button } from 'react-native';
+
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
   return (
-    <View></View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Button title="Sair" onPress={signOut} ></Button>
+    </View>
   );
 }
 
